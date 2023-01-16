9af10e0592072d6f61b720043d786739830732c8
Jana Dvorakova <dvorakova.jana.vf@gmail.com>
2023-01-16 23:34:53 +0100

    js

--- .eleventy.js
+++ .eleventy.js
@@ -1,13 +1,14 @@
 module.exports = function(eleventyConfig) {
-    // Výchozí výstupní složka je: _site
-    // Zkopírovat images/ do _site/images
-    eleventyConfig.addPassthroughCopy("img");
-    // Zkopírovat css/ to _site/css/
-    eleventyConfig.addPassthroughCopy("scss");
-    eleventyConfig.addPassthroughCopy("script.js");
+  // Výchozí výstupní složka je: _site
+  // Zkopírovat images/ do _site/images
+  eleventyConfig.addPassthroughCopy("img");
+  // Zkopírovat css/ to _site/css/
+  eleventyConfig.addPassthroughCopy("scss");
+  eleventyConfig.addPassthroughCopy("js");
 
-   
-   return {
+ 
+  // ...necháme, co už v konfiguraci je, a přidáme:
+  return {
     // možné formáty šablon
     templateFormats: ["njk", "html", "md"],
     // jako šablonovací jazyk zvolíme Nunjucks`
@@ -15,6 +16,8 @@ module.exports = function(eleventyConfig) {
     htmlTemplateEngine: "njk",
     dataTemplateEngine: "njk",
   }
-}
+  }
+;
+  
 
    
