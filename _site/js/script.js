bfc9a6e0ac8908a1ff864419f102ea01b76af41f
Jana Dvorakova <dvorakova.jana.vf@gmail.com>
2023-02-25 15:14:24 +0100

    CNS

--- _site/js/script.js
+++ _site/js/script.js
@@ -182,3 +182,15 @@ function myFunction(imgs) {
   expandImg.parentElement.style.display = "block";
 }
 
+
+  if (window.netlifyIdentity) {
+    window.netlifyIdentity.on("init", user => {
+      if (!user) {
+        window.netlifyIdentity.on("login", () => {
+          document.location.href = "/admin/";
+        });
+      }
+    });
+  }
+
+
