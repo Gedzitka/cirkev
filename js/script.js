65e7c6df44a63d1153584371ac4f9152e09d0c7f
Jana Dvorakova <dvorakova.jana.vf@gmail.com>
2023-04-07 23:10:08 +0200

    header

--- js/script.js
+++ js/script.js
@@ -41,11 +41,11 @@ hamburger.addEventListener("click",function (){
   )
   
   /* When the user scrolls down, hide the navbar. When the user scrolls up, show the navbar */
-  var prevScrollpos = window.pageYOffset;
+  let prevScrollpos = window.pageYOffset;
   window.onscroll = function() {
-    var currentScrollPos = window.pageYOffset;
-    if (prevScrollpos > currentScrollPos) {
-      document.getElementById("header").style.top = "0";
+    let currentScrollPos = window.pageYOffset;
+    if (prevScrollpos >= currentScrollPos) {
+      document.getElementById("header").style.top = "0px";
     } else {
       document.getElementById("header").style.top = "-200px";
     }
