a9f05b0445a883dad93f03bdb2e781cdbde15dbf
Jana Dvorakova <dvorakova.jana.vf@gmail.com>
2023-09-02 14:50:07 +0200

    kkk

--- js/script.js
+++ js/script.js
@@ -93,17 +93,7 @@ hamburger.addEventListener("click",function (){
   })
 
 })
-//galerie
-document.querySelectorAll(".image_container img").forEach(image=>{
-  image.onclick=()=>{
-   document.querySelector(".popup-image").style.display= "block";
-   document.querySelector(".popup-image img").src = image.getAttribute("src")
-  }
 
-})
-document.querySelector(".popup-image span").onclick=()=>{
-   document.querySelector(".popup-image").style.display= "none"
-}
 
 
 
